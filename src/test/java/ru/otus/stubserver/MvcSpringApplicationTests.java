61c52f8de771602042340691500dbff28fcf72f2
Vlasenko Dmitry Andreevich <d.vlasenko@robo.finance>
2023-05-25 16:51:24 +0700

    init

---
+++ src/test/java/ru/otus/stubserver/MvcSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.stubserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
